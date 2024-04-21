24beb8428848e727910a52b53d618c74e7f0a376
Inna Dmytrenko <inna.shalun@gmail.com>
2024-04-21 16:27:48 +0300

    finish HW2

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,9 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+// (union type)
+let union: string | number;
+
+// (literal type)
+type LiteralType = 'enable' | 'disable'
+let literal: LiteralType;
